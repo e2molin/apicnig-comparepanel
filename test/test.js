d9fdde9e49d855673aa443f93af509b63dad9b40
e2molin <esteban.emolin@gmail.com>
2022-07-08 16:26:48 +0200

    (fixing) preparando entorno para testear Timeline

--- test/test.js
+++ test/test.js
@@ -247,6 +247,42 @@ const SENTINELlistBaseLayersByString = [
 ];/* 40 capas */
 
 
+const lyrsTestingTimeline = [
+  [
+    "NACIONAL 1981-1986",
+    "1986",
+    "WMS*NACIONAL_1981-1986*https://www.ign.es/wms/pnoa-historico*NACIONAL_1981-1986",
+  ],
+  [
+    "OLISTAT",
+    "1998",
+    "WMS*OLISTAT*https://www.ign.es/wms/pnoa-historico*OLISTAT",
+  ],
+  ["SIGPAC", "2003", "WMS*SIGPAC*https://www.ign.es/wms/pnoa-historico*SIGPAC"],
+  [
+    "PNOA 2004",
+    "2004",
+    "WMS*pnoa2004*https://www.ign.es/wms/pnoa-historico*pnoa2004",
+  ],
+  [
+    "PNOA 2005",
+    "2005",
+    "WMS*pnoa2005*https://www.ign.es/wms/pnoa-historico*pnoa2005",
+  ],
+  [
+    "PNOA 2006",
+    "2006",
+    "WMS*pnoa2006*https://www.ign.es/wms/pnoa-historico*pnoa2006",
+  ],
+  [
+    "PNOA 2010",
+    "2010",
+    "WMS*pnoa2010*https://www.ign.es/wms/pnoa-historico*pnoa2010",
+  ],
+];
+
+
+
 const mpTOC = new M.plugin.FullTOC({
   position: 'TR',
 });
@@ -268,7 +304,7 @@ const pluginComparepanel = new Comparepanel({
   collapsible: true,
   defaultCompareMode: 'mirror',// mirror - curtain - timeline - spyeye
   defaultCompareViz: 1,
-  baseLayers: SENTINELlistBaseLayersByString,
+  baseLayers: lyrsTestingTimeline,
   /*urlcoberturas: 'https://projects.develmap.com/apicnig/pnoahisto/coberturas.geojson',*/
   timelineParams: { 
     animation: true,
